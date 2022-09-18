a583976ba38d030443e1f87be089a79600c66cee
Sarah <vanwars@gmail.com>
2022-09-18 19:21:24 -0400

    updated tutorial06

--- course-files/tutorials/tutorial06/02-theme-switcher/index.js
+++ course-files/tutorials/tutorial06/02-theme-switcher/index.js
@@ -1,7 +1,25 @@
+const defaultTheme = ev => {
+    // your code here.
+};
+
+const oceanTheme = ev => {
+   // your code here.
+};
+
+const desertTheme = ev => {
+   // your code here.
+};
+
+const highContrastTheme = ev => {
+    // your code here.
+}; 
+
 /*
     Hints: 
-    1. Attach click event handlers to all four of the 
-       buttons (#default, #ocean, #desert, and #high-contrast).
-    2. Modify the className property of the body tag 
-       based on the button that was clicked.
+    1. Attach the event handlers (functions) above to the click event
+       of each of the four buttons (#default, #ocean, #desert, 
+        and #high-contrast) in index.html.
+    2. Then, modify the  body of each function so that it
+       sets the className property of the body tag based on 
+       the button that was clicked.
 */
