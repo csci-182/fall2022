b92ee1df6fa371f454c8cc8ce83ed005e2de0c96
Sarah <vanwars@gmail.com>
2022-10-09 07:34:00 -0400

    Posting HW4

--- course-files/homework/hw04/cars/sketch.js
+++ course-files/homework/hw04/cars/sketch.js
@@ -1,49 +1,48 @@
-const car1 = {
-    x: 100,
-    y: 100,
-    width: 200,
-    speed: 4
-};
-
-// const car2 = {
-//     x: 500,
-//     y: 300,
-//     width: 150,
-//     speed: -3
-// };
-
 const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight; 
 
-async function setup() {
+// feel free to change these values as you like!
+const c1 = {
+    x: 100,
+    y: 100,
+    width: 200,
+    speed: 0.2,
+    color: 'hotpink'
+};
+
+
+// required to set up the canvas:
+function setup() {
     createCanvas(canvasWidth, canvasHeight);
 }
 
+
+// animation loop:
 function draw() {
+    // clear the canvas:
     clear();
 
-    car1.x += car1.speed;
-    drawCar(car1.x, car1.y, car1.width, 'hotpink');
+    // move the car:
+    c1.x += c1.speed;
+
+    // redraw the car:
+    drawCar(c1.x, c1.y, c1.width, c1.color);
     
-    // feel free to comment this line out if
-    // it's not useful:
+    // draw the grid (optional -- feel free to remove this line):
     drawGrid(canvasWidth, canvasHeight);
 }
 
+
 function drawCar(x, y, size, fillColor, wheelColor='black') {
+    strokeWeight(0);
     
     // body
     fill(fillColor);
-    strokeWeight(0);
-    // top: 
-    rect(x - size/4, y - (size/5 + size/7), size / 2, size/7);
-    // bottom:
-    rect(x - size/2, y - size/5, size, size/5);
+    rect(x - size/4, y - (size/5 + size/7), size / 2, size/7); // top
+    rect(x - size/2, y - size/5, size, size/5); // bottom
 
-    // Wheels:
+    // wheels:
     fill(wheelColor);
     circle(x - size / 4, y, size / 6);
     circle(x + size / 4, y, size / 6);
-    
-    drawGrid(canvasWidth, canvasHeight);
 }
